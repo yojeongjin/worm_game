d77c14e35c3c1092bbf462ed2f9fbdfefa6d2f26
yojeongjin <osolku@naver.com>
2022-08-26 21:35:10 +0900

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById(canvas)
+const ctx = canvas.getContext('2d')
+
+const score = document.querySelector('.score')
+const highScore = document.querySelector('.high-score')
+const gameBtn = document.querySelector('.game-btn')
+
