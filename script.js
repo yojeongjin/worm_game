ac696dff0baa9b12ba60cb4ed5f6b02f01d91c44
yojeongjin <osolku@naver.com>
2022-08-29 12:35:10 +0900

    Head play 기능 생성
    feat: 지렁이 머리를 생성하여 위치 파악 및 방향전환이 가능케함

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
-const canvas = document.getElementById(canvas)
+const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-const score = document.querySelector('.score')
-const highScore = document.querySelector('.high-score')
-const gameBtn = document.querySelector('.game-btn')
+const $score = document.querySelector('.score')
+const $highScore = document.querySelector('.high-score')
+const $gameBtn = document.querySelector('.game-btn')
+
+let state = {
+  score: 0,
+  highscore: localStorage.getItem('score') || 0,
+  gameover: true,
+  direction: 1,
+  worm: [
+    {x:10, y:10, direction:1},
+    {x:10, y:20, direction:1},
+    {x:10, y:30, direction:1}
+  ]
+}
+
+
+function drawWorm(ctx, x, y, head = false) {
+  ctx.fillStyle = head ? 'rgba(229, 65, 120, 0.929)' : 'rgba(153, 206, 244, 0.498)'
+  ctx.fillRect(x,y,10,10)
+}
+
+function makeWorm() {
+  for (let i=state.worm.length-1; i>=0; i--) {
+    drawWorm(ctx, state.worm[i].x, state.worm[i].y, i === 0)
+  }
+}
+
+
+
+function moveWorm() {
+  let x = state.worm[0].x
+  let y = state.worm[0].y
+  
+  if (state.direction === 1) {
+    x += 10
+  } else if (state.direction === 2) {
+    y += 10
+  } else if (state.direction === 3) {
+    x -= 10
+  } else if (state.direction === 4) {
+    y -= 10
+  }
+
+  const worm = [{ x, y, direction: state.direction }]
+  state.worm = worm
+}
+
+gameBtn.onClick = () => {
+  window.requestAnimationFrame(play)
+}
+
+function play(timestamp) {
+  start ++
+  if (timestamp-start > 1000 / 10) {
+    playWorm()
+    setWorm()
+    start = timestamp
+  }
+  window.requestAnimationFrame(play)
+}
 
